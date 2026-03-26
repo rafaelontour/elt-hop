fdc4ecca13e9052aab89048d4c43f0da97cfb828
Rafael <86817231+rafaelontour@users.noreply.github.com>
2026-03-26 16:45:44 -0300

    Add files via upload

---
+++ init_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Cria o banco de dados
+psql -U postgres -d postgres -c "CREATE DATABASE
+BD_PESQUISADOR
+ WITH
+ OWNER = postgres
+ ENCODING = 'UTF8'
+ LC_COLLATE = 'Portuguese_Brazil.1252'
+ LC_CTYPE = 'Portuguese_Brazil.1252'
+ TABLESPACE = pg_default
+ CONNECTION LIMIT = -1
+ IS_TEMPLATE = False;"
